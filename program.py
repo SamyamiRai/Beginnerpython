4c6ef2541eccf88f109474745798ba1c2ed62e67
SamyamiRai <71052390+SamyamiRai@users.noreply.github.com>
2020-10-03 13:23:18 +0530

    added print statement

--- program.py
+++ program.py
@@ -5,6 +5,7 @@ def sumNum(num):
         print("Current Number",i,"Previous Number",previousnumber,"Sum:",sum)
         previousnumber=i
 
+print("Program to find the sum of current number and the previous number")
 print("Printing current and previous number sum in a given range(10)")
 sumNum(10)
 

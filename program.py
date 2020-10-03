bd379a0ba9c50a648a2bf9f28b404d0d151f6eb5
Samyami S Rai <sraisamyami@gmail.com>
2020-10-03 13:36:32 +0530

    function name is changed to all lowercase
    Signed-off-by: Samyami S Rai <sraisamyami@gmail.com>

--- program.py
+++ program.py
@@ -1,11 +1,12 @@
-def sumNum(num):
+def sumnum(num):
     previousnumber=0
     for i in range(num):
         sum=previousnumber + i;
         print("Current Number",i,"Previous Number",previousnumber,"Sum:",sum)
         previousnumber=i
 
+
 print("Program to find the sum of current number and the previous number")
 print("Printing current and previous number sum in a given range(10)")
-sumNum(10)
+sumnum(10)
 
